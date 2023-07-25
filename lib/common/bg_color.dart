d349a59450b51c90eab8cce4a10bee192c476914
Mueez Nadeem <mueeznadeem@gmail.com>
2023-07-25 10:46:02 +0500

    Completed Splash and Sign In Screen

---
+++ lib/common/bg_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color bgColor() {
+  return const Color.fromARGB(255, 125, 226, 187);
+}
---
+++ lib/common/signin_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color signinColorLogin() {
+  return const Color.fromARGB(255, 51, 51, 51);
+}
+
+Color signinColorCreateAcc() {
+  return const Color.fromARGB(80, 255, 255, 250);
+}
